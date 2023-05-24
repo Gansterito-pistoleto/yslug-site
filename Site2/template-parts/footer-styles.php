09d4aac8c3e65e03f0072c35db300407ddaf4227
Bandito <dima.pablov445@gmail.com>
2023-05-24 17:16:17 +0300

    Create footer-styles.php

---
+++ Site2/template-parts/footer-styles.php
@@ -0,0 +1,118 @@
+<!-- footer styles -->
+
+<style>.u-footer .u-sheet-1 {
+  min-height: 126px;
+}
+.u-footer .u-image-1 {
+  width: 87px;
+  height: 87px;
+  margin: 20px auto 0 0;
+}
+.u-footer .u-logo-image-1 {
+  width: 100%;
+  height: 100%;
+}
+.u-footer .u-text-1 {
+  margin: -49px 197px 0 642px;
+}
+.u-footer .u-text-2 {
+  margin: -32px auto 0 451px;
+}
+.u-footer .u-text-3 {
+  margin: -32px auto 0 204px;
+}
+.u-footer .u-social-icons-1 {
+  height: 24px;
+  min-height: 16px;
+  width: 93px;
+  min-width: 68px;
+  white-space: nowrap;
+  margin: -24px 61px 37px auto;
+}
+.u-footer .u-icon-1 {
+  height: 25px;
+}
+.u-footer .u-icon-2 {
+  height: 25px;
+}
+.u-footer .u-icon-3 {
+  color: rgb(0, 122, 185) !important;
+  height: 25px;
+}
+@media (max-width: 1199px) {
+  .u-footer .u-image-1 {
+    width: 87px;
+    height: 87px;
+  }
+  .u-footer .u-text-1 {
+    width: 631px;
+    margin-top: -50px;
+    margin-left: 542px;
+    margin-right: 97px;
+  }
+  .u-footer .u-text-2 {
+    margin-top: -32px;
+    margin-left: 372px;
+  }
+  .u-footer .u-text-3 {
+    margin-top: -32px;
+    margin-left: 204px;
+  }
+  .u-footer .u-social-icons-1 {
+    width: 93px;
+  }
+}
+@media (max-width: 991px) {
+  .u-footer .u-text-1 {
+    margin-left: 419px;
+    margin-right: 0;
+  }
+  .u-footer .u-text-2 {
+    margin-left: 285px;
+  }
+}
+@media (max-width: 767px) {
+  .u-footer .u-image-1 {
+    margin-left: 230px;
+  }
+  .u-footer .u-text-1 {
+    width: 540px;
+    margin-top: -72px;
+    margin-left: 0;
+  }
+  .u-footer .u-text-2 {
+    margin-top: 62px;
+    margin-left: 214px;
+  }
+  .u-footer .u-text-3 {
+    margin-top: -61px;
+    margin-left: 153px;
+  }
+  .u-footer .u-social-icons-1 {
+    margin-top: -72px;
+    margin-right: auto;
+    margin-bottom: 42px;
+  }
+}
+@media (max-width: 575px) {
+  .u-footer .u-image-1 {
+    margin-left: auto;
+  }
+  .u-footer .u-text-1 {
+    width: 340px;
+    margin-top: -65px;
+    margin-left: 39px;
+  }
+  .u-footer .u-text-2 {
+    margin-top: 54px;
+    margin-left: 135px;
+  }
+  .u-footer .u-text-3 {
+    margin-top: -61px;
+    margin-left: 96px;
+  }
+  .u-footer .u-social-icons-1 {
+    margin-top: -65px;
+    margin-bottom: 37px;
+  }
+}</style>
